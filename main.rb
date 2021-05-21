4f789be21d92b80ec6602392a90b5e9d1ce752ea
Sesso Kosga <kosgasesso@gmail.com>
2021-05-21 11:42:49 +0100

    Initialise arrays: - digits - ranks - labels - dividers

--- main.rb
+++ main.rb
@@ -1,1 +1,19 @@
-digiys=[]
+#-----------------------#
+#	Number To Letters	#
+#-----------------------#
+
+#Initialise arrays
+#digits one to tem
+digis=['zero','one','two','three','four','five',
+  'six','seven','eight','nine','ten']
+
+#Rank
+ranks=['thirt','fourt','fift']
+
+#Label
+labels=['hundred','thiusand','milliom','billion']
+
+#dividers
+dividers=[100,1000,1_000_000,1_000_000_000]
+
+
